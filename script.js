9cd27c77aa3b7907072a4712e80484c58646140a
Spunei Artur <arthurspunei@gmail.com>
2023-02-24 17:37:44 +0200

    Completed operations tab motion

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const nav__links = document.querySelector('.header__column--2');
 const sections = document.querySelectorAll('.section');
 const targetImages = document.querySelectorAll('img[data-src]');
+const operationsTabs = document.querySelectorAll('.operations__tab');
+const operationsContents = document.querySelectorAll(
+  '.operations__content--container-grid'
+);
 // ------------------------------------------- Smooth scrolling to page sections -------------------------------------------
 nav__links.addEventListener('click', function (e) {
   //console.log(e.target.dataset.id);
@@ -56,3 +60,31 @@ const imgObserver = new IntersectionObserver(callBackImages, {
 targetImages.forEach(img => {
   imgObserver.observe(img);
 });
+
+// ------------------------------------------- Operations tab selection -------------------------------------------
+
+operationsTabs.forEach(tab => {
+  tab.addEventListener('click', function (e) {
+    operationsTabs.forEach(tab =>
+      tab.classList.remove('operations__tab--active')
+    );
+    // Changing Content
+    tab.classList.add('operations__tab--active');
+    const elementClassName = tab.dataset.tab__class;
+    operationsContents.forEach(contentDiv => {
+      contentDiv.classList.remove(
+        'operations__content--container-grid--active'
+      );
+    });
+    document
+      .querySelector(`.${elementClassName}`)
+      .classList.add('operations__content--container-grid--active');
+  });
+});
+
+// ------------------------------------------- Slider -------------------------------------------
+const sliderBtnRight = document.querySelector('.slider__btn--1');
+const sliderBtnLeft = document.querySelector('.slider__btn--2');
+
+// Adding event handler for arrow buttons
+sliderBtnLeft.addEventListener('click', function (e) {});
